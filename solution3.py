f16566cea3e529c39c66d9eadb2dfe8fefa79359
Don Reeves <donjreeves@gmail.com>
2022-05-21 13:40:51 -0400

    first commit

--- solution3.py
+++ solution3.py
@@ -1,1 +1,12 @@
 # Write code for algorithm 3 below
+
+def fibonacci(n):
+    if n == 0:
+        return n
+    if n == 1:
+        return 0
+    if n == 2:
+        return 1
+    return (fibonacci(n-1) + fibonacci(n-2))
+
+print(fibonacci(4))
